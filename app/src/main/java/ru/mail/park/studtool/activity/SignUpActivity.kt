b1e6b7b102503331d43acf209313a307f31b478e
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-08 11:32:45 +0300

    code cleanup

--- app/src/main/java/ru/mail/park/studtool/activity/SignUpActivity.kt
+++ app/src/main/java/ru/mail/park/studtool/activity/SignUpActivity.kt
@@ -6,7 +6,10 @@ import android.content.Intent
 import android.os.AsyncTask
 import android.os.Bundle
 import android.view.View
-import android.widget.*
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ProgressBar
+import android.widget.ScrollView
 import kotlinx.android.synthetic.main.activity_login.*
 import ru.mail.park.studtool.R
 import ru.mail.park.studtool.api.AuthApiManager
