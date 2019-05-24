856b389afdb63c0f00165e1736cf4f8c5758f075
Denis Zinovyev <zinoviev.denis@yandex.ru>
2019-05-24 10:55:58 +0300

    documentsList() api

--- app/src/main/java/ru/mail/park/studtool/api/DocumentsApiManager.kt
+++ app/src/main/java/ru/mail/park/studtool/api/DocumentsApiManager.kt
@@ -6,6 +6,7 @@ import okhttp3.RequestBody
 import ru.mail.park.studtool.auth.AuthInfo
 import ru.mail.park.studtool.document.DocumentInfo
 import ru.mail.park.studtool.exception.InternalApiException
+import ru.mail.park.studtool.exception.NotFoundApiException
 
 class DocumentsApiManager : ApiManager() {
 
@@ -28,6 +29,33 @@ class DocumentsApiManager : ApiManager() {
                 200 -> {
                     val info = fromJSON(it.body()!!.string(), DocumentInfo::class.java)
                     return info as DocumentInfo
+                }
+
+                else -> {
+                    throw InternalApiException(it.body()?.string())
+                }
+            }
+        }
+    }
+
+    fun getDocumentsList(subject: String, authInfo: AuthInfo): Array<DocumentInfo> {
+        val authHeader = getAuthHeader(authInfo)
+
+        val request = Request.Builder()
+            .url("$PROTECTED_REQUEST_V0_PREFIX/documents?owner_id=${authInfo.userId}&subject=$subject")
+            .header(authHeader.first, authHeader.second)
+            .get()
+            .build()
+
+        mClient.newCall(request).execute().use {
+            when (it.code()) {
+                200 -> {
+                    val info = fromJSON(it.body()!!.string(), Array<DocumentInfo>::class.java)
+                    return info as Array<DocumentInfo>
+                }
+
+                404 -> {
+                    throw NotFoundApiException(it.body()?.string())
                 }
 
                 else -> {
