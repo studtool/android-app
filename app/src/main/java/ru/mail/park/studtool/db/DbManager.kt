1cc353c8dc31182620f26c4d49e9c7e28f4b6645
denis96z <zinoviev.denis@yandex.ru>
2019-06-27 17:17:51 +0300

    db manage

---
+++ app/src/main/java/ru/mail/park/studtool/db/DbManager.kt
@@ -0,0 +1,21 @@
+package ru.mail.park.studtool.db
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+class DocumentDbManager(ctx: Context) :
+    SQLiteOpenHelper(ctx, DB_NAME, null, DB_VERSION) {
+    override fun onCreate(db: SQLiteDatabase?) {
+        db?.execSQL(
+            "CREATE TABLE document (id, name, content)"
+        )
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {}
+
+    companion object {
+        private const val DB_NAME = "documents"
+        private const val DB_VERSION = 1
+    }
+}
